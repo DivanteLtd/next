5d0af59dafabe14d4dbae63128dd45b2524e168f
Filip Rakowski <frakowski@divante.pl>
2020-02-26 12:56:22 +0100

    fix footer

--- packages/core/theme-module/theme/components/AppFooter.vue
+++ packages/core/theme-module/theme/components/AppFooter.vue
@@ -108,14 +108,11 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~@storefront-ui/shared/styles/variables";
-
 @mixin for-desktop {
   @media screen and (min-width: $desktop-min) {
     @content;
   }
 }
-
 #footer {
   /* temporary */
   @include for-desktop {
